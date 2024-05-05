aa06d2818e4e832ea4fb445494e88e32ddd5b7f5
DangTrong_DUT <ndtrg281@gmail.com>
2024-05-05 19:36:40 +0700

    ver6 phan chon chuyen bay hoan thien ap dung duoc cho mb a321 a320

--- assets/events/formchonchongoi.js
+++ assets/events/formchonchongoi.js
@@ -125,3 +125,278 @@ function quayLaiChonChoNgoi() {
    
     return true;
 }
+
+
+function taoMaTranChoNgoichuyendiA32X(sogheThuongGia, soCotThuongGia, soghePhoThong1, soghePhoThong2,soghePhoThong3, soCotPhoThong) {
+var html = '';
+
+// Tạo phần thương gia
+var Chimuc = 'A'
+var sohang = 1
+html += '<div class=" maTranViTriChoNgoi__thuonggia">';
+html += '<div class="maTranViTriChoNgoi__dong">';
+for (let index = 0; index < soCotThuongGia+1; index++) {
+    if (index!=soCotThuongGia/2) {
+        html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc">'+Chimuc+'</div>';
+        Chimuc=  String.fromCharCode(Chimuc.charCodeAt(0) +1) 
+    }
+    else
+        html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc"></div>';
+}
+html += '</div>';
+
+for (var i = 0; i < sogheThuongGia/soCotThuongGia; i++) {
+    html += '<div class="maTranViTriChoNgoi__dong">';
+    for (var j = 0; j < soCotThuongGia+1; j++) {
+        if ((((i==parseInt(sogheThuongGia/soCotThuongGia)) &&(j==0||j==soCotThuongGia))||(i==0 && (j==parseInt(soCotThuongGia/2)+1)))&&(parseInt(soCotThuongGia)%soCotThuongGia)==3) {
+            html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc"></div>';
+            console.log("1")
+        } else if (((i==0 && (j==parseInt(soCotThuongGia/2)+1)))&&(parseInt(soCotThuongGia)%soCotThuongGia)==5) 
+        {
+            html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc"></div>';
+        } else if (((i==parseInt(soCotThuongGia/soCotThuongGia)) &&(j==0||j==soCotThuongGia))&&(parseInt(soCotThuongGia)%soCotThuongGia)==4) {
+            html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc"></div>';
+        } else if (j!=parseInt(soCotThuongGia/2))
+            html += '<div class="maTranViTriChoNgoi__dong__phantu ghethuonggiachuyendi" id="TG1' + ''+(i + 1) +'' + (j + 1) + '"></div>';
+        else
+            html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc">'+sohang++ +'</div>';
+    }
+    html += '</div>';
+}
+html += '</div>';
+
+
+
+// Tạo phần phổ thông
+
+Chimuc='A'
+html += '<div class=" maTranViTriChoNgoi__phothong">';
+    html+= '<div class="cuaRaVao">';
+        html+='<div class="cuaRaVao__phantu"><i class="fa-solid fa-circle-exclamation"></i> Cửa thoát hiểm</div> ';
+        html+='<div class="cuaRaVao__phantu"> Cửa thoát hiểm <i class="fa-solid fa-circle-exclamation"></i></div> ';
+    html+='</div>'
+html += '<div class="maTranViTriChoNgoi__dong">';
+for (let index = 0; index < soCotPhoThong+1; index++) {
+    if (index!=soCotPhoThong/2){
+        html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc">'+Chimuc +'</div>';
+        Chimuc=  String.fromCharCode(Chimuc.charCodeAt(0) +1)  
+    }
+    else
+        html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc"></div>';
+}
+html += '</div>';
+
+console.log(parseInt(soghePhoThong1/soCotPhoThong))
+//phan 1 ghe pho thong
+for (var i = 0; i < parseInt(soghePhoThong1/soCotPhoThong)+1; i++) {
+    html += '<div class="maTranViTriChoNgoi__dong">';
+    for (var j = 0; j < soCotPhoThong+1; j++) {
+        if ((((i==parseInt(soghePhoThong1/soCotPhoThong)) &&(j==0||j==soCotPhoThong))||(i==0 && (j==parseInt(soCotPhoThong/2)+1)))&&(parseInt(soghePhoThong1)%soCotPhoThong)==3) {
+            html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc"></div>';
+            console.log("1")
+        } else if (((i==0 && (j==parseInt(soCotPhoThong/2)+1)))&&(parseInt(soghePhoThong1)%soCotPhoThong)==5) 
+        {
+            html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc"></div>';
+        } else if (((i==parseInt(soghePhoThong1/soCotPhoThong)) &&(j==0||j==soCotPhoThong))&&(parseInt(soghePhoThong1)%soCotPhoThong)==4) {
+            html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc"></div>';
+        } else if (j!=parseInt(soCotPhoThong/2))
+            html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phantu--phothong ghephothongchuyendi" id="PT1' + ''+(i + 1) +'' + (j + 1) + '"></div>';
+        else
+            html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc">'+sohang++ +'</div>';
+    }
+    html += '</div>';
+}
+
+html+= '<div class="cuaRaVao">';
+    html+='<div class="cuaRaVao__phantu"><i class="fa-solid fa-circle-exclamation"></i> Cửa thoát hiểm</div> ';
+    html+='<div class="cuaRaVao__phantu"> Cửa thoát hiểm <i class="fa-solid fa-circle-exclamation"></i></div> ';
+html+='</div>'
+
+//phan 2 ghe pho thong
+for (var i = 0; i < parseInt(soghePhoThong2/soCotPhoThong)+1; i++) {
+    html += '<div class="maTranViTriChoNgoi__dong">';
+    for (var j = 0; j < soCotPhoThong+1; j++) {
+        if ((((i==parseInt(soghePhoThong2/soCotPhoThong)) &&(j==0||j==soCotPhoThong))||(i==0 && (j==parseInt(soCotPhoThong/2)+1)))&&(parseInt(soghePhoThong2)%soCotPhoThong)==3) {
+            html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc"></div>';
+            console.log("1")
+        } else if (((i==0 && (j==parseInt(soCotPhoThong/2)+1)))&&(parseInt(soghePhoThong2)%soCotPhoThong)==5) 
+        {
+            html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc"></div>';
+        } else if (((i==parseInt(soghePhoThong2/soCotPhoThong)) &&(j==0||j==soCotPhoThong))&&(parseInt(soghePhoThong2)%soCotPhoThong)==4) {
+            html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc"></div>';
+        } else if (j!=parseInt(soCotPhoThong/2))
+            html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phantu--phothong  ghephothongchuyendi" id="PT2' + ''+(i + 1) +'' + (j + 1) + '"></div>';
+        else
+            html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc">'+sohang++ +'</div>';
+    }
+    html += '</div>';
+}
+
+html+= '<div class="cuaRaVao">';
+    html+='<div class="cuaRaVao__phantu"><i class="fa-solid fa-circle-exclamation"></i> Cửa thoát hiểm</div> ';
+    html+='<div class="cuaRaVao__phantu"> Cửa thoát hiểm <i class="fa-solid fa-circle-exclamation"></i></div> ';
+html+='</div>'
+
+//phan 3 ghe pho thong
+for (var i = 0; i < parseInt(soghePhoThong3/soCotPhoThong)+1; i++) {
+    html += '<div class="maTranViTriChoNgoi__dong">';
+    for (var j = 0; j < soCotPhoThong+1; j++) {
+        if ((((i==parseInt(soghePhoThong3/soCotPhoThong)) &&(j==0||j==soCotPhoThong))||(i==0 && (j==parseInt(soCotPhoThong/2)+1)))&&(parseInt(soghePhoThong3)%soCotPhoThong)==3) {
+            html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc"></div>';
+            console.log("1")
+        } else if (((i==0 && (j==parseInt(soCotPhoThong/2)+1)))&&(parseInt(soghePhoThong3)%soCotPhoThong)==5) 
+        {
+            html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc"></div>';
+        } else if (((i==parseInt(soghePhoThong3/soCotPhoThong)) &&(j==0||j==soCotPhoThong))&&(parseInt(soghePhoThong3)%soCotPhoThong)==4) {
+            html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc"></div>';
+        } else if (j!=parseInt(soCotPhoThong/2))
+            html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phantu--phothong  ghephothongchuyendi" id="PT3' + ''+(i + 1) +'' + (j + 1) + '"></div>';
+        else
+            html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc">'+sohang++ +'</div>';
+    }
+    html += '</div>';
+}
+html += '</div>';
+
+document.getElementById('maTranViTriChoNgoicd').innerHTML = html;
+return html;
+}
+
+
+// chuyen ve
+function taoMaTranChoNgoichuyenveA32X(sogheThuongGia, soCotThuongGia, soghePhoThong1, soghePhoThong2,soghePhoThong3, soCotPhoThong) {
+    var html = '';
+    
+    // Tạo phần thương gia
+    var Chimuc = 'A'
+    var sohang = 1
+    html += '<div class=" maTranViTriChoNgoi__thuonggia">';
+    html += '<div class="maTranViTriChoNgoi__dong">';
+    for (let index = 0; index < soCotThuongGia+1; index++) {
+        if (index!=soCotThuongGia/2) {
+            html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc">'+Chimuc+'</div>';
+            Chimuc=  String.fromCharCode(Chimuc.charCodeAt(0) +1) 
+        }
+        else
+            html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc"></div>';
+    }
+    html += '</div>';
+    
+    for (var i = 0; i < sogheThuongGia/soCotThuongGia; i++) {
+        html += '<div class="maTranViTriChoNgoi__dong">';
+        for (var j = 0; j < soCotThuongGia+1; j++) {
+
+            if ((((i==parseInt(sogheThuongGia/soCotThuongGia)) &&(j==0||j==soCotThuongGia))||(i==0 && (j==parseInt(soCotThuongGia/2)+1)))&&(parseInt(soCotThuongGia)%soCotThuongGia)==3) {
+                html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc"></div>';
+                console.log("1")
+            } else if (((i==0 && (j==parseInt(soCotThuongGia/2)+1)))&&(parseInt(soCotThuongGia)%soCotThuongGia)==5) 
+            {
+                html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc"></div>';
+            } else if (((i==parseInt(soCotThuongGia/soCotThuongGia)) &&(j==0||j==soCotThuongGia))&&(parseInt(soCotThuongGia)%soCotThuongGia)==4) {
+                html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc"></div>';
+            } else if (j!=parseInt(soCotThuongGia/2))
+                html += '<div class="maTranViTriChoNgoi__dong__phantu   ghethuonggiachuyenve" id="TG1' + ''+(i + 1) +'' + (j + 1) + '"></div>';
+            else
+                html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc">'+sohang++ +'</div>';
+        }
+        html += '</div>';
+    }
+    html += '</div>';
+    
+    
+    
+    // Tạo phần phổ thông
+    
+    Chimuc='A'
+    html += '<div class=" maTranViTriChoNgoi__phothong">';
+        html+= '<div class="cuaRaVao">';
+            html+='<div class="cuaRaVao__phantu"><i class="fa-solid fa-circle-exclamation"></i> Cửa thoát hiểm</div> ';
+            html+='<div class="cuaRaVao__phantu"> Cửa thoát hiểm <i class="fa-solid fa-circle-exclamation"></i></div> ';
+        html+='</div>'
+    html += '<div class="maTranViTriChoNgoi__dong">';
+    for (let index = 0; index < soCotPhoThong+1; index++) {
+        if (index!=soCotPhoThong/2){
+            html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc">'+Chimuc +'</div>';
+            Chimuc=  String.fromCharCode(Chimuc.charCodeAt(0) +1)  
+        }
+        else
+            html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc"></div>';
+    }
+    html += '</div>';
+    
+    console.log(parseInt(soghePhoThong1/soCotPhoThong))
+    //phan 1 ghe pho thong
+    for (var i = 0; i < parseInt(soghePhoThong1/soCotPhoThong)+1; i++) {
+        html += '<div class="maTranViTriChoNgoi__dong">';
+        for (var j = 0; j < soCotPhoThong+1; j++) {
+            if ((((i==parseInt(soghePhoThong1/soCotPhoThong)) &&(j==0||j==soCotPhoThong))||(i==0 && (j==parseInt(soCotPhoThong/2)+1)))&&(parseInt(soghePhoThong1)%soCotPhoThong)==3) {
+                html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc"></div>';
+                console.log("1")
+            } else if (((i==0 && (j==parseInt(soCotPhoThong/2)+1)))&&(parseInt(soghePhoThong1)%soCotPhoThong)==5) 
+            {
+                html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc"></div>';
+            } else if (((i==parseInt(soghePhoThong1/soCotPhoThong)) &&(j==0||j==soCotPhoThong))&&(parseInt(soghePhoThong1)%soCotPhoThong)==4) {
+                html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc"></div>';
+            } else if (j!=parseInt(soCotPhoThong/2))
+                html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phantu--phothong  ghephothongchuyenve" id="PT1' + ''+(i + 1) +'' + (j + 1) + '"></div>';
+            else
+                html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc">'+sohang++ +'</div>';
+        }
+        html += '</div>';
+    }
+    
+    html+= '<div class="cuaRaVao">';
+        html+='<div class="cuaRaVao__phantu"><i class="fa-solid fa-circle-exclamation"></i> Cửa thoát hiểm</div> ';
+        html+='<div class="cuaRaVao__phantu"> Cửa thoát hiểm <i class="fa-solid fa-circle-exclamation"></i></div> ';
+    html+='</div>'
+    
+    //phan 2 ghe pho thong
+    for (var i = 0; i < parseInt(soghePhoThong2/soCotPhoThong)+1; i++) {
+        html += '<div class="maTranViTriChoNgoi__dong">';
+        for (var j = 0; j < soCotPhoThong+1; j++) {
+            if ((((i==parseInt(soghePhoThong2/soCotPhoThong)) &&(j==0||j==soCotPhoThong))||(i==0 && (j==parseInt(soCotPhoThong/2)+1)))&&(parseInt(soghePhoThong2)%soCotPhoThong)==3) {
+                html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc"></div>';
+                console.log("1")
+            } else if (((i==0 && (j==parseInt(soCotPhoThong/2)+1)))&&(parseInt(soghePhoThong2)%soCotPhoThong)==5) 
+            {
+                html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc"></div>';
+            } else if (((i==parseInt(soghePhoThong2/soCotPhoThong)) &&(j==0||j==soCotPhoThong))&&(parseInt(soghePhoThong2)%soCotPhoThong)==4) {
+                html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc"></div>';
+            } else if (j!=parseInt(soCotPhoThong/2))
+                html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phantu--phothong  ghephothongchuyenve" id="PT2' + ''+(i + 1) +'' + (j + 1) + '"></div>';
+            else
+                html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc">'+sohang++ +'</div>';
+        }
+        html += '</div>';
+    }
+    
+    html+= '<div class="cuaRaVao">';
+        html+='<div class="cuaRaVao__phantu"><i class="fa-solid fa-circle-exclamation"></i> Cửa thoát hiểm</div> ';
+        html+='<div class="cuaRaVao__phantu"> Cửa thoát hiểm <i class="fa-solid fa-circle-exclamation"></i></div> ';
+    html+='</div>'
+    
+    //phan 3 ghe pho thong
+    for (var i = 0; i < parseInt(soghePhoThong3/soCotPhoThong)+1; i++) {
+        html += '<div class="maTranViTriChoNgoi__dong">';
+        for (var j = 0; j < soCotPhoThong+1; j++) {
+            if ((((i==parseInt(soghePhoThong3/soCotPhoThong)) &&(j==0||j==soCotPhoThong))||(i==0 && (j==parseInt(soCotPhoThong/2)+1)))&&(parseInt(soghePhoThong3)%soCotPhoThong)==3) {
+                html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc"></div>';
+                console.log("1")
+            } else if (((i==0 && (j==parseInt(soCotPhoThong/2)+1)))&&(parseInt(soghePhoThong3)%soCotPhoThong)==5) 
+            {
+                html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc"></div>';
+            } else if (((i==parseInt(soghePhoThong3/soCotPhoThong)) &&(j==0||j==soCotPhoThong))&&(parseInt(soghePhoThong3)%soCotPhoThong)==4) {
+                html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc"></div>';
+            } else if (j!=parseInt(soCotPhoThong/2))
+                html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phantu--phothong  ghephothongchuyenve" id="PT3' + ''+(i + 1) +'' + (j + 1) + '"></div>';
+            else
+                html += '<div class="maTranViTriChoNgoi__dong__phantu maTranViTriChoNgoi__dong__phant--chimuc">'+sohang++ +'</div>';
+        }
+        html += '</div>';
+    }
+    html += '</div>';
+    
+    document.getElementById('maTranViTriChoNgoicv').innerHTML = html;
+    return html;
+    }
+    
