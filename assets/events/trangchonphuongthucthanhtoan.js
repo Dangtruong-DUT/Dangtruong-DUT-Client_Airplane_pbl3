7cf99dcc792985c7eb104ae1ca25ce96b96bb7d6
DangTrong_DUT <ndtrg281@gmail.com>
2024-05-16 19:50:17 +0700

    ver 8.1 bổ sung chức nang ma giam gia

--- assets/events/trangchonphuongthucthanhtoan.js
+++ assets/events/trangchonphuongthucthanhtoan.js
@@ -1,3 +1,37 @@
+
+// Bảng mã giảm giá với số lượng mã giảm giá còn lại
+const discountCodes = {
+    'CODE20': { discount: 20, quantity: 5 },
+    'CODE30': { discount: 30, quantity: 3 },
+    'CODE50': { discount: 50, quantity: 1 }
+  };
+
+$(document).ready(function() {
+    $(".btnSubmit__magiamgia").click(function() {
+      
+        var discountCode = ($(".input__mathanhtoan").val().trim()).toUpperCase();
+        if (discountCodes.hasOwnProperty(discountCode)) {
+            var tongtientamtinh =parseFloat($('.tongtientamtinh1').text()) ;
+            var totalPrice = calculateTotal(tongtientamtinh, discountCode); 
+            var tongkhuyenmai =tongtientamtinh-totalPrice ;
+            $('.tongtienmakhuyenmai').text(tongkhuyenmai);
+            $('.tongtiencanthanhtoan').text(totalPrice);
+        } else {
+            alert('Mã giảm giá không hợp lệ hoặc đã hết.');
+        }
+    });
+});
+  
+  // Hàm tính tổng tiền sau khi giảm giá
+  function calculateTotal(price, code) {
+    if (discountCodes[code] && discountCodes[code].quantity > 0) {
+      const discount = discountCodes[code].discount / 100;
+      return price - (price * discount);
+    } else {
+      alert('Mã giảm giá không hợp lệ hoặc đã hết.');
+      return price;
+    }
+  }
 $(document).ready(function() {
     // Khi thẻ <li> được click
     $('ul.phuongthucthanhtoan li').click(function() {
@@ -15,9 +49,5 @@ $(document).ready(function(){
         $(".magiamgia").toggle();
     });
 });
-$(document).ready(function(){
-    $('.btnSubmit__magiamgia').click(function() {
-       var tongtien= parseFloat($('#tongtientamtinh').text()) - parseFloat($('#tongtienmakhuyenmai').text()) + parseFloat($('#tongtienphidichvu').text())
-       $('#tongtien').text(tongtien)
-    });;
-}); 
+
+  
